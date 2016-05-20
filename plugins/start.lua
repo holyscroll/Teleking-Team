4c66a70609953dfaebf8764656eba35738359f9a
ДGЄИт☆☆☆☆☆FДz <holyscroll@users.noreply.github.com>
2016-05-20 03:39:18 +0000

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'Welcome to BlackPlus!\n\n>To get a new BlackPlus group, contact a support group:\n https://telegram.me/joinchat/A2ETvD160xvoj4y_pNAgig\n\nFor more information, check out our channels:\n\nChannel : @Black_CH\nSite : BlackPlus.ir \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @BlackPlus!'
+local text = 'Welcome to teleking_team!\n\n>To get a new TelekingBlack group, contact a support group:\n https://telegram.me/joinchat/Czbbugf1pE9hf8-jR7sUEg\n\nFor more information, check out our channels:\n\nChannel : @teleking_team\nSite : soon \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @teleking_team!'
 if matches [1] == '/start' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
