7d877635b9cd893f41c199f28d70ff4d52071805
ДGЄИт☆☆☆☆☆FДz <holyscroll@users.noreply.github.com>
2016-05-22 03:00:15 +0000

    Update groupRequest.lua

--- plugins/groupRequest.lua
+++ plugins/groupRequest.lua
@@ -1,13 +1,13 @@
 do
 
  function run(msg, matches)
- local ch = '1040275374'..msg.to.id
+ local ch = ''..msg.to.id
  local fuse = 'New SuperGroup Request!\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username ..'\n\nMessage From: '..msg.to.id.. '\n\nThe Pm:\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
 
 
    local text = matches[1]
-   local chat = "chat#id"..1040275374
+   local chat = "chat#id"..
 
   local sends = send_msg(chat, fuse, ok_cb, false)
   return 'Request Is Sent!\nYour SuperGroup Is Waiting For Create!'
